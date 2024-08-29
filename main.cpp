70b61c745cf20259bb2c4713257233d6304be3a3
Bishal <bishal777giri@gmail.com>
2024-08-29 22:24:53 +0545

    Adding class that evaluates student

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ class Student_PersonalData{
     void getdata_PersonalData();
     void showdata_PersonalData();
 };
-
 void Student_PersonalData::getdata_PersonalData(){
     cout<<"Enter name:"<<endl;
     cin>>name;
@@ -24,6 +23,40 @@ void Student_PersonalData::getdata_PersonalData(){
 }
 void Student_PersonalData::showdata_PersonalData(){
     cout<<"Name:\t"<<name<<"\n"<<"Roll no:\t"<<roll<<"\n"<<"Faculty:\t"<<faculty<<"\n"<<"Phone number:\t"<<phone_no<<"\n"<<"Address:\t"<<address<<endl;
+}
+
+
+class Student_Evaluation:public Student_PersonalData{
+    protected:
+    float marks_first_assement,marks_second_assement,marks_lab_exam,marks_viva;
+    int marks_attendance,marks_assigment,marks_project;
+    public:
+    void getdata_Marks();
+    void calculate_Marks();
+};
+/*Marks distribition is as follows:
+attendance=10%      -10 marks
+first assesment=25% -25 marks
+second assement=25% -25 marks
+Assignment=10%      -10 marks
+project=10%         -10 marks
+lab exam=10%        -10 marks
+viva=10%            -10 marks
+*/
+void Student_Evaluation::getdata_Marks(){
+    cout<<"Enter marks of first and second assesment:"<<endl;
+    cin>>marks_first_assement>>marks_second_assement;
+    cout<<"Enter marks of lab exam and viva:";
+    cin>>marks_lab_exam,marks_viva;
+    cout<<"Enter marks of attendance"<<endl;
+    cin>>marks_attendance;
+    cout<<"Enter marks of assignment and project:"<<endl;
+    cin>>marks_assigment>>marks_project;
+}
+void Student_Evaluation::calculate_Marks(){
+    float temp;
+    temp=marks_assigment+marks_attendance+marks_first_assement+marks_second_assement+marks_lab_exam+marks_project+marks_viva;
+    cout<<"The total internal marks is :"<<temp<<"Out of 100"<<endl;
 }
 
 int main(){
